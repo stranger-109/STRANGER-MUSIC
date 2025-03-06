a2ab4509876ae221b30774ce46b6a293bd1c10d2
stranger-109 <abhishekbhardwaj2425@gmail.com>
2025-03-06 19:38:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Shubhxspam/STRANGER-MUSIC",
+    "https://github.com/stranger-109/STRANGER-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
